099dba4c6e9a2aee475aa5289bc2b0c96d189439
Eivind Bergman <eivind.bergman@gmail.com>
2016-11-23 23:34:06 +0100

    Main body
    main body

--- js/scroll.js
+++ js/scroll.js
@@ -9,4 +9,35 @@ $(document).ready(function() {
 	});
 	// End of smooth scroll
 
+
+	function scrollToAnchor(aid){
+		$('html, body').animate({
+			scrollTop: aid.offset().top
+		},1500);
+	}
+
+	var i = 1;
+		$(document).keydown(function (event) {
+			if (event.keyCode == 38) {
+				i--;
+				scrollToAnchor($('#section'+i+''));
+			} else if (event.keyCode == 40) {
+				i++;
+				event.preventDefault();
+				scrollToAnchor($('#section'+i+''));
+			}
+		});
+
+/*	// Fixed header after scroll
+   $(document).scroll(function() {
+      var nav = $("#nav"),
+            height = $(window).scrollTop();
+      if(height >= ) {
+         nav.addClass('fixed');
+      }else {
+         nav.removeClass('fixed');
+      }
+   });
+   // End of fixed header
+*/
 });
